f804c365e4632bd6cb8c6e7f5ca9c4e6706b2a2b
Arun <arunraghavssreeragam@gmail.com>
2024-09-26 16:30:05 +0530

    finished data generators (1:03:45)

--- jobs/main.py
+++ jobs/main.py
@@ -1,22 +1,23 @@
 import os
+import uuid
 from confluent_kafka import SerializingProducer
 import simplejson as json
-from datetime import datetime
+from datetime import datetime, timedelta
 import random
 
 
 IITJODHPUR_COORDINATES = {
-    "latiude": 26.472059, 
+    "latitude": 26.472059, 
     "longitude": 73.114016
 }
 
 DELHI_COORDINATES = {
-    "latiude": 28.551141,  
+    "latitude": 28.551141,  
     "longitude": 77.088411
 }
 
 #Calculate the increment in latitude and longitude
-LATITUDE_INCREMENT = (DELHI_COORDINATES["latiude"] - IITJODHPUR_COORDINATES["latiude"]) / 100
+LATITUDE_INCREMENT = (DELHI_COORDINATES["latitude"] - IITJODHPUR_COORDINATES["latitude"]) / 100
 LONGITUDE_INCREMENT = (DELHI_COORDINATES["longitude"] - IITJODHPUR_COORDINATES["longitude"]) / 100
 
 #Environment Variables
@@ -27,9 +28,91 @@ TRAFFIC_TOPIC = os.environ.get("TRAFFIC_TOPIC", "traffic_data")
 WEATHER_TOPIC = os.environ.get("WEATHER_TOPIC", "weather_data")
 EMERGENCY_TOPIC = os.environ.get("EMERGENCY_TOPIC", "emergency_data")
 
-
+random.seed(42)
 startTime = datetime.now()
 startLocation = IITJODHPUR_COORDINATES.copy()
+
+def get_next_time():
+    global startTime
+    startTime+=timedelta(seconds=random.randint(30,60))
+    return startTime
+
+
+def generate_vehicle_data(deviceID):
+    '''
+    Generate vehicle data for a given device ID
+    
+    Parameters
+    ----------
+    deviceID : str
+        The ID of the device
+    
+    Returns
+    -------
+    dict
+        The vehicle data containing the device ID and the current location
+    '''
+    location = simulate_vehicle_movement()
+    return{
+        'id':uuid.uuid4(),
+        'deviceID': deviceID,
+        'timestamp': get_next_time().isoformat(),
+        'location': (location['latitude'], location['longitude']),
+        'speed':random.uniform(10,40),
+        'direction':'North-East',
+        'make':'BMW',
+        'model':'C500',
+        'year':2024,
+        'fuelType':'Hybrid'
+        
+        
+    }
+def generate_gps_data(deviceID,timestamp,vehicleType='private'):
+    return{
+        'id':uuid.uuid4(),
+        'deviceID': deviceID,
+        'timestamp': timestamp,
+        'speed':random.uniform(0,40),
+        'direction':'North-East',
+        'vehicleType':vehicleType
+    }
+    
+def generate_traffic_camera_data(deviceID,timestamp,location,cameraID):
+    return{
+        'id':uuid.uuid4(),
+        'deviceID': deviceID,
+        'cameraID': cameraID,
+        'location': location,
+        'timestamp': timestamp,
+        'snapshot':'Base64EncodedString'
+    }
+
+def generate_weather_data(deviceID,timestamp,location):
+    return{
+        'id':uuid.uuid4(),
+        'deviceID': deviceID,
+        'timestamp': timestamp,
+        'location': location,
+        'temperature':random.uniform(-5,34),
+        'weatherCondition':random.choice(['Sunny','Cloudy','Rainy','Storm']),
+        'precipitation':random.uniform(0,25),
+        'windSpeed':random.uniform(0,100),
+        'humidity':random.uniform(0,100),
+        'airQualityIndex':random.uniform(0,500)
+    
+    }
+
+def generate_emergency_incident_data(deviceID,timestamp,location):
+    return{
+        'id':uuid.uuid4(),
+        'deviceID': deviceID,
+        'incidentID': uuid.uuid4(),
+        'type': random.choice(['Fire','Accident','Earthquake','Flood','Medical','Police','None']),
+        'timestamp': timestamp,
+        'location': location,
+        'status':random.choice(['Active', 'Resolved']),
+        'description':'Description of the incident'
+    }
 
 def simulate_vehicle_movement():
     '''
@@ -64,21 +147,7 @@ def simulate_vehicle_movement():
     
     
 
-def generate_vehicle_data(deviceID):
-    '''
-    Generate vehicle data for a given device ID
-    
-    Parameters
-    ----------
-    deviceID : str
-        The ID of the device
-    
-    Returns
-    -------
-    dict
-        The vehicle data containing the device ID and the current location
-    '''
-    location = simulate_vehicle_movement()
+
 
 def simulate_journey(producer, deviceID):
     # When travelling
@@ -90,6 +159,17 @@ def simulate_journey(producer, deviceID):
     
     while True:
         vehicleData = generate_vehicle_data(deviceID)
+        gpsData=generate_gps_data(deviceID,vehicleData['timestamp'])
+        traffic_camera_data=generate_traffic_camera_data(deviceID,vehicleData['timestamp'],vehicleData['location'],'Nikon-Cam123')
+        weather_data=generate_weather_data(deviceID,vehicleData['timestamp'],vehicleData['location'])
+        emergency_incident_data=generate_emergency_incident_data(deviceID,vehicleData['timestamp'],vehicleData['location'])
+        
+        print(f"Publishing Vehicle Data: {vehicleData}")
+        print(f"Publishing GPS Data: {gpsData}")
+        print(f"Publishing Traffic Camera Data: {traffic_camera_data}")
+        print(f"Publishing Weather Data: {weather_data}")
+        print(f"Publishing Emergency Incident Data: {emergency_incident_data}")
+        break
 
 
 if __name__ == "__main__":
